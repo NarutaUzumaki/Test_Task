57e94df929292fcdb935c45e87cc6441423760b1
Pride <orion.nikolaev@gmail.com>
2020-03-07 14:49:55 +0200

    Мелочь2

--- app/data/Config.php
+++ app/data/Config.php
@@ -5,5 +5,5 @@ class Config{
     public static $server = "localhost:3306";
     public static  $user = "root";
     public static  $password = "";
-    public static  $dataBase = "accaunting";
+    public static  $dataBase = "moneyChange";
 }
